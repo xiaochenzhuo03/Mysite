3b114756c287ee12e1506c3eb8e250765be2712f
xiaochenzhuo03 <xiaochenzhuo03@163.com>
2015-05-10 11:29:30 -0700

    Update login.php

--- finance/login.php
+++ finance/login.php
@@ -7,9 +7,9 @@
     exit;
   }
 	$DB_HOST = "localhost";
-	$DB_NAME = "xiaobytg_f";
-	$DB_USERNAME = "xiaobytg_God";
-	$DB_PASSWORD = "0332005b";
+	$DB_NAME = "xxxxxxx";
+	$DB_USERNAME = "xxxxxxxx";
+	$DB_PASSWORD = "xxxxxx";
 	$conn = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME", $DB_USERNAME, $DB_PASSWORD);
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	
